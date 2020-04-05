fa7ba87e41dfa68635aff1bd6bd0c7d96437f9be
Denys Kondakov <dkondakov@users.noreply.github.com>
2020-04-05 18:53:34 +0300

    Added super functions and console log.

--- compose.js
+++ compose.js
@@ -1,7 +1,21 @@
 function compose(...args) {
     return param => {
-        let result;
-        args.forEach(fn => result = fn(result));
+        let result = param;
+        args.forEach(fn => {
+                result = fn(result);
+            }
+            );
         return result;
     };
 }
+
+const superFunction = compose(arg => arg + 1, arg => arg + 2);
+
+const res1 = superFunction(1);
+
+const superPuper = compose(superFunction, arg => arg + 1, arg => arg + 2);
+
+const res2 = superPuper(1);
+
+console.log('result should be 4: ' + res1);
+console.log('result should be 7: ' + res2);
